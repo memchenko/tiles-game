e8247e2b9d3c69a11340786a8684efc3bdad7c91
Емченко Михаил <memchenko@htc-cs.ru>
2019-03-04 16:33:05 +0400

    fix: fixed postcss webpack config

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -26,7 +26,7 @@ module.exports = {
         rules: [
             { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
             { test: /\.pug$/, exclude: /node_modules/, loader: 'pug-loader' },
-            { test: /\.styl$/, exclude: /node_modules/, loader: 'style-loader!css-loader!postcss-loader' }
+            { test: /\.p?css$/, exclude: /node_modules/, loader: 'style-loader!postcss-loader' }
         ]
     },
     plugins: [

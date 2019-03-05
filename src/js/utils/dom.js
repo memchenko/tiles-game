9beb10606fde19265f89edc1979145ac42bfd0cf
Емченко Михаил <memchenko@htc-cs.ru>
2019-03-05 12:09:04 +0400

    feat: dom utils refactor

--- src/js/utils/dom.js
+++ src/js/utils/dom.js
@@ -1,13 +1,9 @@
 import IO from './IO';
 
-export const getElement = (selector) => {
-  return new IO(() => {
-    return document.querySelector(selector);
-  });
-};
+export const getElement = selector =>  new IO(() => {
+  return document.querySelector(selector);
+});
 
-export const get2DContext = (canvasElement) => {
-  return new IO(() => {
-    return canvasElement.getContent('2d');
-  });
-};
+export const get2DContext = canvasElement => new IO(() => {
+  return canvasElement.getContent('2d');
+});

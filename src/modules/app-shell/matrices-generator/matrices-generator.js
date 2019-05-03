ef1b3dd71f96c5d5ba4ec7d92b08c9c67f284ddd
memchenko <mikhail.emchenko@gmail.com>
2019-05-03 22:02:54 +0400

    feat: added function to generate colorful matrix

--- src/modules/app-shell/matrices-generator/matrices-generator.js
+++ src/modules/app-shell/matrices-generator/matrices-generator.js
@@ -1,23 +1,45 @@
 import { converge, compose, join, partialRight, addIndex, map, concat, splitEvery, multiply, thunkify, head, subtract, apply, always } from 'ramda';
-import { random255 } from '../../../utils/random';
 import { trunc, getRoundedDivision, getGte0, multiplyAll } from '../../../utils/math';
 import { toHex, concatStr, getStrLeadingZero2, sliceStr } from '../../../utils/strings';
+import { randomWithin } from '../../../utils/random';
 
-// getRandomFilter :: () -> String
-const getRandomFilter = compose(getStrLeadingZero2, toHex, trunc, random255);
 // getArray :: (Number, a -> b) -> [*]
 const getArray = (length, mapFn) => Array.from({ length }, mapFn);
 // getArrayFromArgs :: (*...) -> [*]
 const getArrayFromArgs = (...args) => args;
 const mapIndexed = addIndex(map);
 
-// getRandomColor :: () -> String
-export const getRandomColor = compose(
-    concatStr('#'),
-    concatStr(getRandomFilter()),
-    concatStr(getRandomFilter()),
-    getRandomFilter
-);
+// hslToRgb :: (Number, Number, Number) -> String
+export function hslToRgb(h, s, l) {
+    let rgb = [0, 0, 0];
+    const channelToHex = compose(getStrLeadingZero2, toHex, trunc, multiply(255));
+    const rgbChannelsToHex = compose(concatStr('#'), join(''), map(channelToHex));
+
+	if (s === 0) {
+		rgb = map(always(l), rgb);
+	} else {
+		const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+		const p = 2 * l - q;
+
+        rgb = [
+            hueToRgbChannel(p, q, h + 1/3),
+            hueToRgbChannel(p, q, h),
+            hueToRgbChannel(p, q, h - 1/3)
+        ];
+    }
+    
+    return rgbChannelsToHex(rgb);
+}
+
+// hueToRgb :: (Number, Number, Number) -> Number
+function hueToRgbChannel(p, q, t) {
+    if (t < 0) t += 1;
+    if (t > 1) t -= 1;
+    if (t < 1/6) return p + (q - p) * 6 * t;
+    if (t < 1/2) return q;
+    if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+    return p;
+}
 
 // getShadesOf :: (String, Number) -> [String]
 export const getShadesOf = (color, num) => {
@@ -60,3 +82,26 @@ export const generateBWMatrix = compose(
     converge(splitEvery, [head, getArrayOfBWTilesWithSpecifiedCoverage]),
     getArrayFromArgs
 );
+
+// generateMonochromeMatrix :: (Number, Number, Number, String) -> [[String]]
+export const generateMonochromeMatrix = (cols, rows, diversity, color) => {
+    const numberOfTiles = cols * rows;
+    const numberOfColors = trunc(diversity * numberOfTiles);
+    const shadesOfColor = getShadesOf(color, numberOfColors + 1).slice(0, -1);
+    const arrayOfColors = getArray(numberOfTiles, (_, i) => {
+        return shadesOfColor[i % numberOfColors];
+    });
+    return splitEvery(cols, arrayOfColors);
+};
+
+// generateColorfulMatrix :: (Number, Number, Number) -> [[String]]
+export const generateColorfulMatrix = (cols, rows, diversity) => {
+    const numberOfTiles = cols * rows;
+    const numberOfColors = trunc(diversity * numberOfTiles);
+    const randomDegree = randomWithin(0, 1);
+    const hueStep = 1 / numberOfColors;
+    const degrees = getArray(numberOfColors, (_, i) => ((randomDegree + hueStep * i) % 1));
+    const colors = degrees.map(degree => hslToRgb(degree, 0.5, 0.5));
+    const arrayOfColors = getArray(numberOfTiles, (_, i) => colors[i % numberOfColors]);
+    return splitEvery(cols, arrayOfColors);
+};
--- src/utils/random.js
+++ src/utils/random.js
@@ -1,2 +1,1 @@
 export const randomWithin = (start = 0, end = 1) => start + Math.random() * (end - start);
-export const random255 = () => randomWithin(0, 255);

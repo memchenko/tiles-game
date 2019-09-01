4d580b3be11172d2640594abd0643a6c603fe7ac
memchenko <mikhail.emchenko@gmail.com>
2019-09-01 22:47:01 +0400

    feat($store): added selectors for maps and results

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,3 +1,5 @@
 import { lensPath } from 'ramda';
 
 export const SCENARIOS_LENS = lensPath(['scenarios', 'descriptions']);
+export const SCENARIOS_MAPS = lensPath(['scenarios', 'maps']);
+export const SCENARIOS_RESULTS = lensPath(['scenarios', 'results']);

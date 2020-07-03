5af0f908b1a928e725fdb4ef24a12bab1cfc6216
Михаил Емченко <memchenko@Mihails-MacBook-Pro.local>
2020-07-04 01:37:50 +0400

    translate levels.js to .ts

--- src/lib/levels/levels.ts
+++ src/lib/levels/levels.ts
@@ -18,66 +18,75 @@ export default class Levels {
         FAIL: 'FAIL'
     };
 
-    _coefficients = {
+    private coefficients = {
         [Levels.LEVELS.EASY]: range(0.4, 0.6),
         [Levels.LEVELS.MEDIUM]: range(0.3, 0.7),
         [Levels.LEVELS.HARD]: range(0.4, 0.8),
         [Levels.LEVELS.EXPERT]: range(0.5, 1)
     };
 
-    _tilesInRowNumber = {
+    private tilesInRowNumber = {
         [Levels.LEVELS.EASY]: range(3, 4),
         [Levels.LEVELS.MEDIUM]: range(5, 7),
         [Levels.LEVELS.HARD]: range(8, 10),
         [Levels.LEVELS.EXPERT]: range(11, 15)   
     };
 
-    _currentState = {
-        level: Levels.LEVELS.EASY,
+    private currentState: {
+        level: keyof typeof Levels['LEVELS'];
+        coefficientPointer: number;
+        tilesInRowPointer: number;
+    } = {
+        level: Levels.LEVELS.EASY as keyof typeof Levels['LEVELS'],
         coefficientPointer: 0,
         tilesInRowPointer: 0
     };
 
-    _matrixType = null;
+    private matrixType: keyof typeof Levels['MATRICES_TYPES'] | null = null;
 
-    _successRate = 1;
+    private successRate = 1;
 
     constructor({
         startLevel = 'EASY',
         startCoefficient = 0,
         startTilesInRow = 0,
         matrixType = 'SINGLE_COLOR'
+    }: {
+        startLevel?: keyof typeof Levels['LEVELS'];
+        startCoefficient?: number;
+        startTilesInRow?: number;
+        matrixType?: keyof typeof Levels['MATRICES_TYPES'];
     } = {}) {
         this.setCurrentLevel(startLevel, startCoefficient, startTilesInRow);
         this.setMatrixType(matrixType);
     }
 
-    updateLevels(updater) {
-        if (updater === Levels.UPDATERS.SUCCESS && this._successRate < 1) {
-            this._successRate = 1;
+    updateLevels(updater: keyof typeof Levels[]) {
+        if (updater === Levels.UPDATERS.SUCCESS && this.successRate < 1) {
+            this.successRate = 1;
         }
 
         if (updater === Levels.UPDATERS.FAIL) {
-            this._successRate /= 2;
+            this.successRate /= 2;
         }
     }
 
     updateCurrentState() {
-        if (this._successRate < 0.3) {
+        if (this.successRate < 0.3) {
             this.downgrade();
         }
 
-        if (this._successRate === 1) {
+        if (this.successRate === 1) {
             this.raise();
         }
     }
 
     raise() {
-        const { level, coefficientPointer, tilesInRowPointer } = this._currentState;
-        const coefficientsRange = this._coefficients[level];
-        const tilesInRowRange = this._tilesInRowNumber[level];
+        const { level, coefficientPointer, tilesInRowPointer } = this.currentState;
+        const coefficientsRange = this.coefficients[level];
+        const tilesInRowRange = this.tilesInRowNumber[level];
 
-        const levels = Object.keys(Levels.LEVELS);
+        const levels = Object.keys(Levels.LEVELS) as (keyof typeof Levels['LEVELS'])[];
         const isLastCoefficient = coefficientsRange.length === coefficientPointer + 1;
         const isLastTilesInRow = tilesInRowRange.length === tilesInRowPointer + 1;
         const isLastLevel = level === levels[levels.length - 1];
@@ -93,10 +102,10 @@ export default class Levels {
     }
 
     downgrade() {
-        const { level, coefficientPointer, tilesInRowPointer } = this._currentState;
-        const coefficientsRange = this._coefficients[level];
+        const { level, coefficientPointer, tilesInRowPointer } = this.currentState;
+        const coefficientsRange = this.coefficients[level];
 
-        const levels = Object.keys(Levels.LEVELS);
+        const levels = Object.keys(Levels.LEVELS) as (keyof typeof Levels['LEVELS'])[];
         const isFirstCoefficient = coefficientPointer === 0;
         const isFirstTilesInRow = tilesInRowPointer === 0;
         const isFirstLevel = level === levels[0];
@@ -107,27 +116,31 @@ export default class Levels {
             this.setCurrentLevel(level, coefficientsRange.length - 1, tilesInRowPointer - 1);
         } else if (isFirstCoefficient && isFirstTilesInRow && !isFirstLevel) {
             const prevLevel = levels[levels.indexOf(level) - 1];
-            const prevLevelCoefficientsRange = this._coefficients[prevLevel];
-            const prevTilesInRowRange = this._tilesInRowNumber[prevLevel];
-            this.setCurrentLevel(prevLevel, prevLevelCoefficientsRange.length - 1, prevTilesInRowRange - 1);
+            const prevLevelCoefficientsRange = this.coefficients[prevLevel];
+            const prevTilesInRowRange = this.tilesInRowNumber[prevLevel];
+            this.setCurrentLevel(prevLevel, prevLevelCoefficientsRange.length - 1, prevTilesInRowRange.length - 1);
         }
     }
 
-    setCurrentLevel(level, coefficientPointer, tilesInRowPointer) {
-        this._currentState = {
+    setCurrentLevel(
+        level: keyof typeof Levels['LEVELS'],
+        coefficientPointer: number,
+        tilesInRowPointer: number,
+    ) {
+        this.currentState = {
             level, coefficientPointer, tilesInRowPointer
         };
     }
 
-    setMatrixType(type) {
-        this._matrixType = type;
+    setMatrixType(type: keyof typeof Levels['MATRICES_TYPES']) {
+        this.matrixType = type;
     }
 
     getMatrixParams() {
-        const { level, coefficientPointer, tilesInRowPointer } = this._currentState;
-        const rows = this._tilesInRowNumber[level][tilesInRowPointer];
+        const { level, coefficientPointer, tilesInRowPointer } = this.currentState;
+        const rows = this.tilesInRowNumber[level][tilesInRowPointer];
         const cols = rows;
-        const coeffiecient = this._coefficients[level][coefficientPointer];
+        const coeffiecient = this.coefficients[level][coefficientPointer];
 
         return [cols, rows, coeffiecient];
     }

9949e6fdf024cb0cf53f12bc0bc1e3a99b73bbe3
Михаил Емченко <mikhail.emchenko@gmail.com>
2020-07-05 00:43:58 +0400

    chore(entity): add mock

--- src/entities/scenario/reducers.ts
+++ src/entities/scenario/reducers.ts
@@ -3,8 +3,41 @@ import { createReducer } from 'deox';
 import { IScenarioState } from './types';
 import { setScenario, updateScenario } from './actions';
 
+import { Difficulties } from '../../constants/game';
+
 const initialState: IScenarioState = {
-    list: [],
+    list: [
+        {
+            id: 0,
+            order: 0,
+            icon: 'https://avatars.dicebear.com/v2/jdenticon/birds.svg',
+            title: 'Explore Birds',
+            difficulty: Difficulties.Easy,
+            poster: 'https://avatars.dicebear.com/v2/jdenticon/birds.svg',
+            progress: 0,
+            description: 'Learn more about birds species with "Explore Brids" puzzles',
+        },
+        {
+            id: 1,
+            order: 1,
+            icon: 'https://avatars.dicebear.com/v2/jdenticon/trees.svg',
+            title: 'Explore Trees',
+            difficulty: Difficulties.Easy,
+            poster: 'https://avatars.dicebear.com/v2/jdenticon/trees.svg',
+            progress: 0.33,
+            description: 'Learn more about trees species with "Explore Trees" puzzles',
+        },
+        {
+            id: 2,
+            order: 2,
+            icon: 'https://avatars.dicebear.com/v2/jdenticon/space.svg',
+            title: 'Explore Space',
+            difficulty: Difficulties.Easy,
+            poster: 'https://avatars.dicebear.com/v2/jdenticon/space.svg',
+            progress: 0.66,
+            description: 'Learn more about Space with "Explore Space" puzzles',
+        },
+    ],
 };
 
 export default createReducer(initialState, handle => ([

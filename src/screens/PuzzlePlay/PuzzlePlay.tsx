15a2fcf325012b693fed6a48a2184b328809a361
Михаил Емченко <mikhail.emchenko@gmail.com>
2020-07-05 02:17:55 +0400

    feat(screens): add PuzzlePlay

--- src/screens/PuzzlePlay/PuzzlePlay.tsx
+++ src/screens/PuzzlePlay/PuzzlePlay.tsx
@@ -1,3 +1,42 @@
-export default function PuzzlesPlay() {
-    return null;
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { __, curry } from 'ramda';
+
+import {
+    generateBWMatrix,
+    generateMonochromeMatrix,
+    generateColorfulMatrix
+} from '../../lib/matrices-generator/matrices-generator';
+import { AppRoutes } from '../../constants/urls';
+import TilesGrid from '../../components/TilesGrid';
+import TilesGridInteractive from '../../components/TilesGridInteractive';
+import { TileInfo } from '../../lib/grid/types';
+
+const routeMatricesMap = (color: string) => {
+    return {
+        [AppRoutes.PlayBW]: generateBWMatrix,
+        [AppRoutes.PlayMonochrome]: curry(generateMonochromeMatrix)(__, __, __, color),
+        [AppRoutes.PlayColorful]: generateColorfulMatrix
+    };
+};
+
+function logChanges(mtx: TileInfo[][]) {
+    console.log(mtx); // eslint-disable-line
+}
+
+export default function PuzzlePlay() {
+    const { pathname, state } = useLocation();
+    const generateMtx: any = routeMatricesMap(state);
+    const [mtx] = useState(generateMtx[pathname](3, 3, .5) as TileInfo[][]);
+
+    return mtx
+        ? <div style={{ display: 'flex' }}>
+            <div style={{ width: '70%' }}>
+                <TilesGridInteractive matrix={mtx} onMatrixChange={logChanges} />
+            </div>
+            <div style={{ width: '30%' }}>
+                <TilesGrid matrix={mtx} />
+            </div>
+        </div>
+        : null;
 }

1832a314570a454e692e31ac58954e84ee567fec
Михаил Емченко <mikhail.emchenko@gmail.com>
2020-07-04 20:31:09 +0400

    feat(screens): add MainMenu markup

--- src/screens/MainMenu/MainMenu.tsx
+++ src/screens/MainMenu/MainMenu.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { AppRoutes } from '../../constants/urls';
 
 export default function MainMenu() {
-    return null;
+    return (
+        <ul>
+            <li>
+                <Link to={ AppRoutes.Scenario }>Сценарии</Link>
+            </li>
+            <li>
+                <Link to={ AppRoutes.Play }>Играть</Link>
+            </li>
+        </ul>
+    );
 }

83ba7677be6c6c8f5795142200f36dd427a5aa81
Михаил Емченко <mikhail.emchenko@gmail.com>
2020-07-07 23:33:42 +0400

    feat(screens): ScenarioMap working

--- src/screens/ScenarioMap/ScenarioMap.tsx
+++ src/screens/ScenarioMap/ScenarioMap.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useRef, RefObject } from 'react';
+import React, { useEffect, useRef, useState, RefObject } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { view, lensProp, compose } from 'ramda';
 import { History } from 'history';
 
-import { getScenarioResults } from '../../entities/result';
-import { get, requestsLens, IRequestData, RequestStatus } from '../../entities/network';
-import { getMapByScenarioId, IMapInfo } from '../../entities/map';
+import { getScenarioResults, IStateWithResult, IStepsResults } from '../../entities/result';
+import { getAsset, requestsLens, IRequestData, RequestStatus } from '../../entities/network';
+import { getMapByScenarioId, IMapInfo, IStateWithMap } from '../../entities/map';
 import { ScenarioMapCssClasses, ScenarioMapDataAttrs, Results } from '../../constants/game';
 import { AssetsPaths, AppRoutes } from '../../constants/urls';
 
@@ -35,50 +35,49 @@ function setLevelsResults(
     history: History,
     mapContainer: RefObject<HTMLDivElement>,
 ) {
-    return () => {
-        if (!mapContainer.current) return;
-        
-        const levelsNodes = document.querySelectorAll(`[data-${ScenarioMapDataAttrs.Level}]`);
-        Array.prototype.forEach.call(levelsNodes, (node: HTMLElement) => {
-            const key = snakeToCamel(ScenarioMapDataAttrs.Level);
-            const stepNumData = node.dataset[key];
+    if (!mapContainer.current) return;
+    
+    const levelsNodes = document.querySelectorAll(`[data-${ScenarioMapDataAttrs.Level}]`);
+    Array.prototype.forEach.call(levelsNodes, (node: HTMLElement) => {
+        const key = snakeToCamel(ScenarioMapDataAttrs.Level);
+        const stepNumData = node.dataset[key];
 
-            if (!stepNumData) {
-                throw new Error('No html element');
-            }
+        if (!stepNumData) {
+            throw new Error('No html element');
+        }
 
-            const stepNumberFromHTML = +stepNumData;
-            const css = node.classList;
-            const { openOnStepSolved = null } = map.find(item => item.id === stepNumberFromHTML) || {};
-            const isAddDisabled = stepNumberFromHTML > 0
-                && openOnStepSolved !== null
-                && !results.hasOwnProperty(String(openOnStepSolved));
+        const stepNumberFromHTML = +stepNumData;
+        const css = node.classList;
+        const { openOnStepSolved = null } = map.find(item => item.id === stepNumberFromHTML) || {};
+        const isAddDisabled = stepNumberFromHTML > 0
+            && openOnStepSolved !== null
+            && !results.hasOwnProperty(String(openOnStepSolved));
 
-            if (isAddDisabled) {
-                css.add(ScenarioMapCssClasses.Disabled);
-            } else if (results.hasOwnProperty(stepNumberFromHTML)) {
-                const type = results[stepNumberFromHTML];
+        if (isAddDisabled) {
+            css.add(ScenarioMapCssClasses.Disabled);
+        } else if (results.hasOwnProperty(stepNumberFromHTML)) {
+            const type = results[stepNumberFromHTML];
 
-                css.add(PERFORMANCE_TO_CSS_CLASS[type]);
-            }
+            css.add(PERFORMANCE_TO_CSS_CLASS[type]);
+        }
 
-            if (!isAddDisabled) {
-                node.addEventListener('click', () => {
-                    const urlScenarioId = getUrlWithPathParams(AppRoutes.ScenarioPlay, {
-                        scenarioId,
-                        stepNumber: String(stepNumberFromHTML),
-                    });
-                
-                    history.push(urlScenarioId);
+        if (!isAddDisabled) {
+            node.addEventListener('click', () => {
+                const urlScenarioId = getUrlWithPathParams(AppRoutes.ScenarioPlay, {
+                    scenarioId,
+                    stepId: String(stepNumberFromHTML),
                 });
-            }
-        });
-    };
+            
+                history.push(urlScenarioId);
+            });
+        }
+    });
 }
 
 const key = lensProp('scnearioMapRequest');
 
 export default function ScenarioMap() {
+    const [html, setHtml] = useState('');
     const dispatch = useDispatch();
     const mapContainer = useRef(null);
     const { scenarioId } = useParams();
@@ -89,39 +88,56 @@ export default function ScenarioMap() {
             view(requestsLens)
         )
     ) as IRequestData<string>;
-    const results = useSelector(getScenarioResults(scenarioId));
-    const map = useSelector(getMapByScenarioId(scenarioId));
+    const results = useSelector<
+        IStateWithResult,
+        IStepsResults
+    >(getScenarioResults(scenarioId));
+    const map = useSelector<
+        IStateWithMap,
+        IMapInfo[]
+    >(getMapByScenarioId(scenarioId));
 
     useEffect(() => {
         const headers = new Headers();
         headers.append('Content-Type', 'text/plain;charset=utf-8');
 
         dispatch(
-            get({
+            getAsset({
                 key,
                 path: AssetsPaths.Map,
                 pathParams: { scenarioId },
                 headers,
             })
         );
-    }, []);
+    }, [dispatch, scenarioId]);
 
-    useEffect(
+    useEffect(() => {
+        if (req && req.status === RequestStatus.Success && req.data) {
+            (req.data as unknown as Blob).text().then(text => setHtml(text));
+        }
+    }, [req, html]);
+
+    useEffect(() => {
         setLevelsResults(
             scenarioId,
             results,
             map,
             history,
             mapContainer,
-        ),
-        []
-    );
+        )
+    }, [scenarioId, results, map, history, mapContainer, html]);
+
+    if (!req) {
+        return (
+            <div>Suspense...</div>
+        );
+    }
 
     return (
         req.status === RequestStatus.Fail
             ? <div>Error</div>
-            : !req.data || req.status === RequestStatus.Pending
+            : !req.data || req.status === RequestStatus.Pending || !html
             ? <div>Suspense...</div>
-            : <div ref={ mapContainer } dangerouslySetInnerHTML={{ __html: req.data }}></div>
+            : <div ref={ mapContainer } dangerouslySetInnerHTML={{ __html: html }}></div>
     );
 }

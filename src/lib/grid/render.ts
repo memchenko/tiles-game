a8a1cfc8cf4516eaf0f79b7487bc696a506bfede
Михаил Емченко <mikhail.emchenko@gmail.com>
2020-08-09 20:03:08 +0400

    feat(screens): add grids to PuzzlePlay

--- src/lib/grid/render.ts
+++ src/lib/grid/render.ts
@@ -22,7 +22,7 @@ export const getCanvasGeometry = (
   return { height, width, top, left };
 });
 
-export const setImage = curry(
+export const displayImage = curry(
   (
     ctx: CanvasRenderingContext2D,
     { image, x, y, width, height, ...rest }
@@ -51,23 +51,47 @@ export const setColor = curry(
   )
 );
 
-export const setRectCoords = curry(
+export const displayRect = curry(
   (ctx, { color, x, y, width, height, ...rest }) => new IO(
     () => {
       if (!color) return { x, y, width, height, ...rest };
+      const gutter = width < 100 ? 2 : 3;
+      const displayWidth = roundUpToHalf(width) - gutter * 2;
+      const displayHeight = roundUpToHalf(height) - gutter * 2;
+      const radius = width < 100 ? 5 : 10;
+      const displayX = x + gutter;
+      const displayY = y + gutter;
 
-      ctx.fillRect(x, y, width, height);
+      ctx.beginPath();
+
+      ctx.fillStyle = color;
+
+      ctx.moveTo(displayX + radius, displayY);
+      ctx.quadraticCurveTo(displayX, displayY, displayX, displayY + radius);
+      ctx.lineTo(displayX, displayY + displayHeight - radius);
+      ctx.quadraticCurveTo(displayX, displayY + displayHeight, displayX + radius, displayY + displayHeight);
+      ctx.lineTo(displayX + displayWidth - radius, displayY + displayHeight);
+      ctx.quadraticCurveTo(displayX + displayWidth, displayY + displayHeight, displayX + displayWidth, displayY + displayHeight - radius);
+      ctx.lineTo(displayX + displayWidth, displayY + radius);
+      ctx.quadraticCurveTo(displayX + displayWidth, displayY, displayX + displayWidth - radius, displayY);
+      ctx.closePath();
+      ctx.fill();
+
       return { x, y, width, height, ...rest };
     }
   )
 );
 
+export function roundUpToHalf(num: number) {
+  const result = Math.round(num * 2) / 2;
+  return (result / 0.5) % 1 > 0 ? result : result + 0.5;
+}
+
 export const drawRect = curry((ctx: CanvasRenderingContext2D, params: { color?: string }) =>
-  setColor(ctx, params)[chain](
-    setRectCoords(ctx)
-  )[chain](
-    setImage(ctx)
-  ).unsafePerformIO()
+  setColor(ctx, params)
+    [chain](displayRect(ctx))
+    [chain](displayImage(ctx))
+    .unsafePerformIO()
 );
 
 export const getGridData = (
@@ -104,8 +128,14 @@ export const redrawColumn = curry(
     const appendant = { ...head, y: tail.y + tileHeight };
     const prependant = { ...tail, y: head.y - tileHeight };
     const drawableArr = [prependant].concat(arr.map(el => ({ ...el }))).concat([appendant]);
+    const { x, y, width } = arr[0];
+    const height = ctx.canvas.height;
 
-    requestAnimationFrame(() => drawableArr.forEach(drawRect(ctx)));
+    requestAnimationFrame(() => {
+      ctx.clearRect(x, y, width, height);
+      
+      drawableArr.forEach(drawRect(ctx));
+    });
   }
 );
 
@@ -117,7 +147,13 @@ export const redrawRow = curry(
     const appendant = { ...head, x: tail.x + tileWidth };
     const prependant = { ...tail, x: head.x - tileWidth };
     const drawableArr = [prependant].concat(arr.map(el => ({ ...el }))).concat([appendant]);
+    const { x, y, height } = arr[0];
+    const { width } = ctx.canvas;
 
-    requestAnimationFrame(() => drawableArr.forEach(drawRect(ctx)));
+    requestAnimationFrame(() => {
+      ctx.clearRect(x, y, width, height);
+      
+      drawableArr.forEach(drawRect(ctx));
+    });
   }
 );
--- src/screens/PuzzlePlay/PuzzlePlay.tsx
+++ src/screens/PuzzlePlay/PuzzlePlay.tsx
@@ -1,9 +1,20 @@
 import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import cn from 'classnames';
+
+import './PuzzlePlay.scss';
 
 import Layout from '../../components/Layout';
 import { IconTypes } from '../../components/Icon';
 import { PerformanceTypes } from '../../constants/game';
+import TilesGrid from '../../components/TilesGrid';
+import TilesGridInteractive from '../../components/TilesGridInteractive';
+
+const mtx = [
+    ['#F7567C', '#5D576B', '#EDB88B'].map(color => ({ color })),
+    ['#F7567C', '#5D576B', '#EDB88B'].reverse().map(color => ({ color })),
+    ['#F7567C', '#5D576B', '#EDB88B'].map(color => ({ color })),
+];
 
 export default function PuzzlePlay() {
     const { state } = useLocation<{ isNew: boolean; }>();
@@ -25,7 +36,20 @@ export default function PuzzlePlay() {
                 performanceValue: '1:34',
             }}
         >
-            wow
+            <div className="row-1"></div>
+            <div className={ cn('row-2', 'grid-row') }>
+                <div className={ cn('col-center-2', 'puzzle-play-example') }>
+                    <TilesGrid matrix={ mtx } />
+                </div>
+            </div>
+            <div className={ cn('row-5', 'puzzle-play-area') }>
+                <div className="puzzle-play-area__grid">
+                    <TilesGridInteractive
+                        matrix={ mtx }
+                        onMatrixChange={ () => {} }
+                    />
+                </div>
+            </div>
         </Layout>
     );
 }

79f2b90924df8505278ca05ca752974de24ca80e
Михаил Емченко <mikhail.emchenko@gmail.com>
2020-08-15 21:49:35 +0400

    chore(screens): plug unused screens out

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { AppRoutes } from './constants/urls';
 import MainMenu from './screens/MainMenu';
-import ScenariosMenu from './screens/ScenariosMenu';
-import ScenarioDetails from './screens/ScenarioDetails';
-import ScenarioPlay from './screens/ScenarioPlay';
 import PuzzlePlay from './screens/PuzzlePlay';
 
 function App() {
@@ -15,9 +12,6 @@ function App() {
         <Route path={ AppRoutes.Root } exact>
           <MainMenu />
         </Route>
-        <Route path={ AppRoutes.Scenario } component={ ScenariosMenu } exact />
-        <Route path={ AppRoutes.ScenarioDetails } component={ ScenarioDetails } />
-        <Route path={ AppRoutes.ScenarioPlay } component={ ScenarioPlay } />
         <Route path={ AppRoutes.Play }>
           <PuzzlePlay />
         </Route>

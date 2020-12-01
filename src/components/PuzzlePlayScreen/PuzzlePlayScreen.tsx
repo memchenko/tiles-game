bfefa42cb50fde49d494953a2e52f941fe925f9f
Михаил Емченко <mikhail.emchenko@gmail.com>
2020-12-01 23:49:58 +0400

    remove unused import

--- src/components/PuzzlePlayScreen/PuzzlePlayScreen.tsx
+++ src/components/PuzzlePlayScreen/PuzzlePlayScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
+import React, { useEffect, useRef, useCallback } from 'react';
 import { Switch } from 'react-router-dom';
 import cn from 'classnames';
 
